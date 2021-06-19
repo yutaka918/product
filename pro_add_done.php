c5aa8d44234678be5790b73385ad88915d5200cf
yutaka918 <83869387+yutaka918@users.noreply.github.com>
2021-06-19 13:04:49 +0900

    Add files via upload

---
+++ pro_add_done.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>ろくまる農園</title>
+</head>
+<body>
+    <?php
+        try {
+            $pro_name   = $_POST['name'];
+            $pro_price  = $_POST['price'];
+            $pro_gazou_name = $_POST['gazou_name'];
+
+            $pro_name   = htmlspecialchars($pro_name,ENT_QUOTES,'UTF-8');
+            $pro_price  = htmlspecialchars($pro_price,ENT_QUOTES,'UTF-8');
+
+            //↓下記の5行がデータベースに接続するためのプログラム　
+            $dsn         = 'mysql:dbname=mst_product;host=localhost;charset=utf8';   
+            $user        = 'root';
+            $password    = '';
+            $dbh         = new PDO($dsn, $user, $password);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql         = 'INSERT INTO mst_product(name,price,gazou) VALUES(?,?,?)';
+            $stmt        = $dbh->prepare($sql);
+            $data[]      = $pro_name;
+            $data[]      = $pro_price;
+            $data[]      = $pro_gazou_name;
+            $stmt->execute($data);
+
+            $dbh         = null;
+
+            print $pro_name;
+            print 'を追加しました。<br>';
+
+        } catch(Exception $e) {
+            print 'ただいま障害により大変ご迷惑をお掛けしております。';
+            exit();
+        }
+    ?>
+
+    <a href="pro_list.php">戻る</a>
+
+</body>
+</html>
